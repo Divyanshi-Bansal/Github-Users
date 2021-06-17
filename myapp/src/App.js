a7789a2eafc116bb83418e7e13c027203572d649
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-06-17 22:50:48 +0530

    partially header complete

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/header';
 
 function App() {
   return (
     <div>
-      
+      <Header/>
     </div>
   );
 }
